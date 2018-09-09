981b0590d4caf95f5b74637487b7e13beeb5efe3
AngelSoulstar <AngelSoulstar@users.noreply.github.com>
2018-09-09 22:11:20 +0200

    test from home

--- D04/ex01/create.php
+++ D04/ex01/create.php
@@ -3,8 +3,11 @@ if ($_POST['login'] != null && $_POST['passwd'] != null && $_POST['submit'] == '
     if (!file_exists('./private')) {
         mkdir('private');
     }
-
+    $tmp = unserialize(file_get_contents('./private/passwd'));
+    $tmp[] = array($_POST['login'] => $_POST['passwd']);
+    file_put_contents('./private/passwd', serialize($tmp));
     echo ("OK");
 } else {
     echo ("ERROR");
 }
+// hash('sha256', 
