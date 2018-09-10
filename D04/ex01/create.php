258a50203813eb74f0b7765410780cc5c849f36d
Kevin Erbault <kerbault@z2r7p1.le-101.fr>
2018-09-10 19:04:58 +0200

    Ex01 done

--- D04/ex01/create.php
+++ D04/ex01/create.php
@@ -1,10 +1,22 @@
 <?php
-if ($_POST['login'] != null && $_POST['passwd'] != null && $_POST['submit'] == 'OK') {
+if (isset($_POST['login']) && isset($_POST['passwd']) && isset($_POST['submit']) && $_POST['login'] != null && $_POST['passwd'] != null && $_POST['submit'] == 'OK') {
     if (!file_exists('./private')) {
         mkdir('private');
     }
-
-    echo ("OK");
+    $tmp['login'] = $_POST['login'];
+    $tmp['passwd'] = hash("sha512",$_POST['passwd']);
+    if (file_exists('./private/passwd')) {
+        $users = unserialize(file_get_contents("./private/passwd"));
+        foreach ($users as $tmp2) {
+            if ($tmp2['login'] == $tmp['login']) {
+                echo "ERROR\n";
+                exit;
+            }
+        }
+    }
+    $users[] = $tmp;
+    file_put_contents('./private/passwd', serialize($users));
+    echo "OK\n";
 } else {
-    echo ("ERROR");
+    echo "ERROR\n";
 }
